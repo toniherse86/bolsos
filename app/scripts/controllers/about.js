b520a7b1839d7cac23d8f701706098197233c037
toniherse86 <toniherse86@gmail.com>
2016-01-20 22:25:25 +0100

    bolsos

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bolsosApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bolsosApp
+ */
+angular.module('bolsosApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bolsosApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bolsosApp
+ */
+angular.module('bolsosApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
